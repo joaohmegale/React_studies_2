17ae2f557738dc0493a7a483cf3cec39b6a8a167
joaohmegale <joaohmegale@gmail.com>
2023-09-04 18:35:20 -0300

    formCommit2

--- src/App.js
+++ src/App.js
@@ -2,38 +2,33 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [email, setEmail] = useState('teste@teste.com')
-  const [senha, setSenha] = useState('123123')
-  const [sexo, setSexo] = useState({sexo: 'masculino'})
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
 
-  const trocaEmail = (e) =>{
-    setEmail(e.target.value);
-  }
+  const cadastrar = (e) =>{
+    
+    alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`);
 
-  const trocaSexo = (e) =>{
-    setSexo(e.target.value);
+    e.preventDefault();
   }
 
   return (
     <div>
-      <h2>Login</h2>
-      Email:
-      <input type='email' name='email' value={email} 
-          onChange={trocaEmail}/>
-      <br />
-      Senha: 
-      <input type = 'password' name='senha' value={senha}
-          onChange={(e) => setSenha(e.target.value) }/>
-      Sexo:
-      <select name='sexo' value={sexo} onChange={trocaSexo}>
-        <option value='masculino'>masculino</option>
-        <option value='feminino'>feminino</option>
-      </select>
-      <div>
-        <h3>{email}</h3>
-        <h3>{senha}</h3>
-        <h3>{sexo}</h3>
-      </div>
+      <h1>Novo usuario</h1>
+      <form onSubmit={cadastrar}>
+        <input type='text' placeholder='nome' value={nome} onChange={(e) => setNome(e.target.value)}/>
+        <br/>
+        <hr/>
+        <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <br/>
+        <hr/>
+        <input type='password' placeholder='senha' value={senha} onChange={(e) => setSenha(e.target.value)}/>
+        <hr/>
+        
+        <button type='submit'>Cadastrar</button>
+      </form>
+      
     </div>
   );
 

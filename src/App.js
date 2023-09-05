3bf0e5ec183cde45cc7e1b3acd611b221b1a2c5d
joaohmegale <joaohmegale@gmail.com>
2023-09-05 14:30:28 -0300

    luckyCookieCommit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import { useState } from 'react';
+import  Botao  from './components/Botao/index'
 import './App.css';
 
 function App() {
-  const [nome, setNome] = useState('')
-  const [email, setEmail] = useState('')
-  const [senha, setSenha] = useState('')
+  const [frases, setFrases] = useState([
+    'Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
+    'O riso é a menor distância entre duas pessoas.', 
+    'Deixe de lado as preocupações e seja feliz.',
+    'Realize o óbvio, pense no improvável e conquiste o impossível.',
+    'Acredite em milagres, mas não dependa deles.',
+    'A maior barreira para o sucesso é o medo do fracasso.'
+    ]);
 
-  const cadastrar = (e) =>{
-    
-    alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`);
+    const[textoFrase, setTextoFrase] = useState('');
 
-    e.preventDefault();
-  }
+    const quebraBiscoito = () =>{
+      let numeroRandom = Math.floor(Math.random() * frases.length);
+      setTextoFrase('"' + frases[numeroRandom] + '"');
+    } 
 
   return (
-    <div>
-      <h1>Novo usuario</h1>
-      <form onSubmit={cadastrar}>
-        <input type='text' placeholder='nome' value={nome} onChange={(e) => setNome(e.target.value)}/>
-        <br/>
-        <hr/>
-        <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <br/>
-        <hr/>
-        <input type='password' placeholder='senha' value={senha} onChange={(e) => setSenha(e.target.value)}/>
-        <hr/>
-        
-        <button type='submit'>Cadastrar</button>
-      </form>
-      
+    <div className='container'>
+      <img className='img' src={require('./components/assets/biscoito.png')}/>
+      <Botao nome="Abrir biscoito" acaoBtn={quebraBiscoito}/>
+      <h3 className='textoFrase'>{textoFrase}</h3>
     </div>
   );
 
 }
+
 export default App;
